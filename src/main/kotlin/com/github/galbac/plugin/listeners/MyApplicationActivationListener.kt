d99ce1ac90b693e713e27dcdfd1b984e1be5c8e5
GitHub Action <action@github.com>
2023-11-14 14:17:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/galbac/plugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/galbac/plugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.galbac.plugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
